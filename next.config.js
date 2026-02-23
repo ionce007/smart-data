7d7fd50390a1f5df3d169623c2f9e9e0783fdffe
ionce007 <ionce@163.com>
2026-02-24 00:32:55 +0800

    打包方式问题04

--- next.config.js
+++ next.config.js
@@ -15,6 +15,16 @@ const nextConfig = {
   },
   // 可选：显式指定 Webpack 配置（确保 Webpack 生效）
   webpack: (config, { dev, isServer }) => {
+    if (!isServer) {
+      // 客户端不打包 node 模块
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        sequelize: false,
+      };
+    }
     // 保留原有 Webpack 配置（如有），无需额外修改
     return config;
   },
